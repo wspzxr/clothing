746d9cba23bb63320ccb9fccffced2f6bacc8dba
lvpeng <1441255608@qq.com>
2020-01-11 17:01:33 +0800

    死亡测试

--- clothing/admin/admin.js
+++ clothing/admin/admin.js
@@ -3,11 +3,11 @@ const router =express.Router()
 
 const Login = require('./adminRouter/adminUserRouter')
 const Root = require('./adminRouter/adminRootRouter')
-const Food = require('./adminRouter/adminFoodRouter')
+const Book = require('./adminRouter/adminFoodRouter')
 // 用户登录相关
 router.use('/user',Login)
 // 权限管理相关
 router.use('/root',Root)
 // 商品管理相关
-router.use('/food',Food)
+router.use('/book',Book)
 module.exports=router
--- clothing/control/foodController.js
+++ clothing/control/foodController.js
@@ -1,8 +1,8 @@
 // 存放和食品 数据操作的相关信息 数据库的操作
-const FoodModel= require('../db/model/foodModel')
-async function  add(name,price,img,foodType,desc){
+const FoodModel= require('../db/model/bookModel')
+async function  add(title,permission,arttype,settop){
   // async 函数内部只要不出错 肯定走的是then 如果出错走的是catch
-   let result =await FoodModel.insertMany({name,price,img,foodType,desc})
+   let result =await FoodModel.insertMany({title,permission,arttype,settop})
    console.log(result)
 }
 async function get(page,pageSize){
@@ -10,8 +10,8 @@ async function get(page,pageSize){
   let allFoods =await FoodModel.find()
   // 获取视食品数据 总数量
   let allCount =allFoods.length 
-  let foods = await FoodModel.find().skip((page-1)*pageSize).limit(pageSize)
-  return  {foods,allCount}
+  let books = await FoodModel.find().skip((page-1)*pageSize).limit(pageSize)
+  return  {books,allCount}
 }
 
 // 分类查询+分页
@@ -37,9 +37,9 @@ async function del(foodId){
 }
 
 // 修改
-async function  update(foodId,name,price,img,foodType,desc){
+async function  update(foodId,title,permission,arttype,settop){
   
-  let result  = await FoodModel.updateOne({_id:foodId},{name,price,img,foodType,desc})
+  let result  = await FoodModel.updateOne({_id:foodId},{title,permission,arttype,settop})
    console.log(result)
    return  result
 }
